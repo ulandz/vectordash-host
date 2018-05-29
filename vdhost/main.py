c4e1446be439e18a69b60d347ed310247ccba8b1
Sharif Shameem <samin100@gmail.com>
2018-05-28 20:05:36 -0400

    idk

--- vdhost/main.py
+++ vdhost/main.py
@@ -4,7 +4,6 @@ from vdhost.cli.install import install
 from vdhost.cli.launch import launch
 from vdhost.cli.login import login
 from vdhost.cli.set_commands import set_commands
-from vdhost.cli.set_gpu_ids import set_gpu_ids
 from vdhost.cli.start_miner import start_miner
 from vdhost.cli.stop_miner import stop_miner
 
@@ -25,7 +24,6 @@ def add_commands(cli):
     cli.add_command(launch)
     cli.add_command(login)
     cli.add_command(set_commands)
-    cli.add_command(set_gpu_ids)
     cli.add_command(start_miner)
     cli.add_command(stop_miner)
 
