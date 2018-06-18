8484d0fcc525b01065042d178b58fdfa0bb4ce64
Sharif Shameem <samin100@gmail.com>
2018-06-17 20:35:18 -0400

    refactored start_hosting.py

--- vdhost/cli/start_hosting.py
+++ vdhost/cli/start_hosting.py
@@ -10,7 +10,6 @@ from os import environ
 
 if environ.get('VECTORDASH_BASE_URL'):
     VECTORDASH_URL = environ.get('VECTORDASH_BASE_URL')
-    print('Using development URL:' + VECTORDASH_URL)
 else:
     VECTORDASH_URL = "http://vectordash.com/"
 
@@ -18,68 +17,52 @@ else:
 @click.command(name='start-hosting')
 def start_hosting():
     """
-    args: None | Connects this machine to the Vectordash server.
+    Connects this machine to the Vectordash server.
     """
     try:
-        # Path to vectordash client directory
-        client_dir = '/var/vectordash/client'
-
-        # If the var directory does not exist,
-        if not os.path.isdir(client_dir):
-            print(stylize("Error getting package. You have not ran ", fg("red")) + stylize("vdhost login", fg("blue")))
-            exit(0)
 
         # if the installation has not been completed
         if not os.path.isfile('/var/vectordash/install_complete'):
-            print("Installation has not been completed/ Please run " +
+            print("The Vectordash client has not been installed. Please run " +
                   stylize("sudo vdhost install", fg("blue")))
-            exit(0)
+            return
 
-        # path to login file
-        login_file = os.path.expanduser('/var/vectordash/login.json')
-        
-        try:
-            with open(login_file, 'r') as f:
-                data = json.load(f)
-                email = data["email"]
-                machine_key = data["machine_key"]
-                r = requests.post(VECTORDASH_URL + "authenticate-host/",
-                                  data={'email': email, 'machine_key': machine_key})
-                resp = r.text
-                resp = json.loads(resp)
+        # ensuring the login file exists
+        if not os.path.isfile('/var/vectordash/login.json'):
+            print("You are not logged in. Please run " +
+                  stylize("vdhost login", fg("blue")) + ' to continue.')
+            return
 
-                # if the login failed
-                if not resp['valid_authentication']:
-                    print(stylize("Invalid login, please run ", fg("red")) + stylize("vdhost login", fg("blue")))
-                    exit(0)
+        # opening the credentials JSON
+        with open('/var/vectordash/login.json', 'r') as f:
+            data = json.load(f)
 
-        except Exception as e:
-            print(stylize("An error occurred, Please make sure you have run ", fg("red")) +
-                  stylize("vdhost login ", fg("blue")) +
-                  stylize("and provided the correct email address and machine key.", fg("red")))
-            exit(0)
+        # grabbing the values
+        email = data["email"]
+        machine_key = data["machine_key"]
 
-        if not os.path.isfile("/etc/supervisor/conf.d/vdclient.conf"):
-            print(stylize("Something went wrong during the installation process. Please try running ", fg("red")) +
-                  stylize("vdhost install ", fg("blue")) +
-                  stylize("again.", fg("red")))
-            exit(0)
+        # ensuring the login information is valid
+        r = requests.post(VECTORDASH_URL + "authenticate-host/", data={'email': email, 'machine_key': machine_key})
 
-        else:
-            try:
-                print(stylize("Launching the Vectordash client on this machine...", fg("green")))
-                # Run the client script
-                subprocess.call("sudo supervisorctl start vdclient", shell=True) 
-            
-            except subprocess.CalledProcessError as e:
-                print(stylize("An unexpected error has occured: ", fg("red")) +
-                      stylize(e, fg("red")))
-                exit(0)
+        # getting the response and loading it as a dict
+        resp = r.text
+        resp = json.loads(resp)
 
-            except OSError:
-                print("You do not have permission to execute this. Try re-running the command as sudo: "
-                      + stylize("sudo vdhost start-hosting", fg("blue")))
-                exit(0)
+        # if the login failed we display an error and return
+        if not resp['valid_authentication']:
+            print("Invalid authentication information . Please run " +
+                stylize("vdhost login", fg("blue")) + ' to continue.')
+            return
 
-    except ValueError as e:
-        print(stylize("An unexpected error has occured: " + str(e), fg("red")))
+        # Launching the the client script
+        subprocess.call("sudo supervisorctl start vdclient", shell=True)
+
+        # Printing a success message
+        print(stylize("Vectordash client started.", fg("green")))
+
+    except OSError:  # if we get a permissions error
+        print(stylize("Please run this command as sudo:", fg("red"))
+              + stylize("sudo vdhost start-hosting", fg("blue")))
+
+    except Exception as e:
+        print(stylize("An unexpected error has occurred: " + str(e), fg("red")))
