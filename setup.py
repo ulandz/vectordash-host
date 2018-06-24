1c0500e5c277a5104476810cf082770c640d6a08
erbzz <arbaz.khatib@gmail.com>
2018-06-24 19:41:41 -0400

    colored v 1.3.7 was broken so we depend on version 1.3.5 now

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import os
 from setuptools import find_packages, setup
 
 project = "vdhost"
-version = "0.8.4"
+version = "0.8.6"
 
 with open(os.path.join(os.path.dirname(__file__), 'README.md')) as readme:
     long_description = readme.read()
@@ -23,7 +23,7 @@ setup(
     install_requires=[
         "click>=6.7,<7",
         "requests>=2.18.4",
-        "colored>=1.3.5",
+        "colored==1.3.5",
     ],
     setup_requires=[],
     dependency_links=[],
